267cf75cc31cc81805cd55a0403283aad99c884d
ojpbay <oli.baylis@gmail.com>
2024-10-17 10:12:31 +0100

    Add start of interface for people results

--- src/example/star-wars.service.ts
+++ src/example/star-wars.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
+import { PeopleResponse } from './star-wars.model';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +13,10 @@ export class StarWarsService {
 
   public getPeople(): Observable<any[]> {
     return this.httpClient
-      .get(`${this.BaseUrl}/people`)
-      .pipe(map((res) => [res]));
+      .get<PeopleResponse>(`${this.BaseUrl}/people`)
+      .pipe(
+        tap(x => console.log(x)),
+        map(res => res.results)
+      )      
   }
 }
